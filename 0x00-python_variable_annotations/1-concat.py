5ccec41b53824e2551770be6469c1fc727c05f3f
meriembenayad <meriem.benayad@gmail.com>
2024-03-14 21:44:32 +0000

    Task 1: Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 """ 1. Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Method that returns a concatenated string """
+    return str1 + str2
