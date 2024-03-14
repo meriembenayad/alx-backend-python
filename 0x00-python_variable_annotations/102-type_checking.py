e2b66dad311b8d2cd6e07bef68663d70d01bc11f
meriembenayad <meriem.benayad@gmail.com>
2024-03-14 23:03:58 +0000

    Task 12: Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,1 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" 12. Type Checking """
+from typing import Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """ Use mypy to validate the following code """
+    zoomed_in: Tuple = tuple(
+        item for item in lst
+        for i in range(factor)
+    )
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
