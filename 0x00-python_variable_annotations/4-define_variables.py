7315f8293f65f946e147cc3008d73bff2bfcc869
meriembenayad <meriem.benayad@gmail.com>
2024-03-14 22:00:26 +0000

    Task 4: Define variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" 4. Define variables """
+
+# Annonate an integer with a value
+a: int = 1
+
+# Annonate a float with a value
+pi: float = 3.14
+
+# Annonate a boolean with a value
+i_understand_annotations: bool = True
+
+# Annonate a string with a value
+school: str = "Holberton"
