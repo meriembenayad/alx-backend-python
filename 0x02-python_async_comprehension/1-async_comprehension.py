aa42b476cd1556e9135da42c885855d9939e0219
meriembenayad <meriem.benayad@gmail.com>
2024-03-19 20:56:28 +0000

    Task 1: Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""" 0. Async Generator """
+import asyncio
+import random
+from typing import Generator
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """
+    Coroutine collect 10 random numbers
+    using an async comprehensing over async_generator
+    then return the 10 random numbers.
+    """
+    return [i async for i in async_generator()]
