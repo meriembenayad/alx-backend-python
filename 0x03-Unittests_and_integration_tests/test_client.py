eba93bf85181318edc64e6686060345ad73c5a9b
meriembenayad <meriem.benayad@gmail.com>
2024-04-12 17:08:04 +0000

    Task 4: Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,17 +9,17 @@ from paramertized import parameterized
 class TestGithubOrgClient(unittest.TestCase):
     """ Test case class """
     @parameterized.expand([
-        ("google"),
-        ("abc")
+        ("google",),
+        ("abc",)
     ])
     @patch("client.get_json", return_value={"payload": True})
-    def test_org(self, name_org: str, mock_get: Mock) -> None:
+    def test_org(self, org_name: str, mock_get: Mock) -> None:
         """
         Test that GithubOrgClient.org returns the correct value
         """
-        test_class = GithubOrgClient(name_org)
+        test_class = GithubOrgClient(org_name)
         self.assertEqual(test_class.org, {"payload": True})
-        url = f"https://api.github.com/orgs/{name_org}"
+        url = f"https://api.github.com/orgs/{org_name}"
 
         # Test that get_json was called once with expected arg
         mock_get.assert_called_once_with(url)
