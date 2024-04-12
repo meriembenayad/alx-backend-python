3969cd5a53f87ff81eb24e3b3581c8104f7d9cf5
meriembenayad <meriem.benayad@gmail.com>
2024-04-12 15:01:36 +0000

    Task 2: Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """ 0. Parameterize a unit test """
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import Dict, Any, Tuple
 from parameterized import parameterized
+from unittest.mock import patch, Mock
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ Test case for the access_nested_map function """
+    """ 0. Test case for the access_nested_map function """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -15,7 +17,8 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map: Dict[str, Any],
                                path: Tuple[str], expected: Any) -> None:
-        """ Test Method that uses the parameterized.expand decorator
+        """
+            0. Test Method that uses the parameterized.expand decorator
             to test the access_nested_map function with different inputs
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
@@ -27,9 +30,24 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map: Dict[str, Any],
                                          path: Tuple[str]) -> None:
         """
-            Test Method to test access_nested_map function
+            1. Test Method to test access_nested_map function
             using decorator @parameterized.expand with different inputs
             that should raise a KeyError
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ 2. Test case for the get_json Method """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url: str,
+                      test_payload: Dict[str, Any], mock_get: Mock) -> None:
+        """ 2. Test methof to test get_json method """
+        mock_get.return_value.json.return_value = test_payload
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get.assert_called_once_with(test_url)
