4a3c72c2fd3a1a0bec7c1711213c51c110823e05
meriembenayad <meriem.benayad@gmail.com>
2024-04-12 17:23:12 +0000

    Task 5: Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ 4. Parameterize and patch as decorators """
 from client import GithubOrgClient
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 import unittest
 from parameterized import parameterized
 
@@ -23,3 +23,17 @@ class TestGithubOrgClient(unittest.TestCase):
 
         # Test that get_json was called once with expected arg
         mock_get.assert_called_once_with(url)
+
+    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_get: Mock) -> None:
+        """ Method to unit-test GithubOrgClient._public_repos_url """
+        payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+        mock_get.return_value = payload
+        # create an instance of GitHubOrgClient
+        client = GithubOrgClient("google")
+        # Call the public_repos_url method
+        pub_repos = client._public_repos_url
+
+        # Assert (Check the result is as expected)
+        self.assertEqual(
+            pub_repos, payload['repos_url'])
